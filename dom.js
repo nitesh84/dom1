2fd12bd361abbe103344fd3862d8b4b49b1e5c52
Nitesh Wadhwani <niteshwadhwani2019@gmail.com>
2024-10-17 23:57:48 +0530

    First commit

---
+++ dom.js
@@ -0,0 +1,10 @@
+let divContainer=document.getElementById("container");
+
+console.log(divContainer)
+divContainer.style.height="200px";
+divContainer.style.width="300px";
+divContainer.style.backgroundColor="yellow";
+divContainer.style.margin="20px";
+divContainer.style.padding="10px";
+divContainer.style.font="18px";
+divContainer.style.fontWeight="bold";
